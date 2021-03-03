1a1fa33314980567a2bd6f2735ea8075da56f35e
Kerry Snyder <kerry@atmanaut.me>
2021-03-03 02:10:20 -0800

    firestore not database… why didn't that break anything?

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	plugins: [vue()],
 	optimizeDeps: {
-		include: ['firebase/app', 'firebase/auth', 'firebase/database'],
+		include: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
 		exclude: ['path'],
+	},
+	build: {
+		chunkSizeWarningLimit: 1000,
 	},
 })
