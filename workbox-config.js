4a627e00661fed1b33606106ea54e8968cd8291e
Kerry Snyder <kerry@atmanaut.me>
2021-05-15 19:54:35 -0700

    service worker for offline a2hs-ability

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	globDirectory: 'public/',
+	globPatterns: ['**/*.{svg,jpg,xml,png,ico,json}'],
+	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/, /^source/],
+	maximumFileSizeToCacheInBytes: 4800000,
+	swDest: 'public/sw.js',
+}
