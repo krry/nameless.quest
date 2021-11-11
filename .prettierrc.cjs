229fcccb6a7008c8397d80c9b46f7427d46333fb
Kerry Snyder <kerry@atmanaut.me>
2021-11-11 10:43:03 -0800

    sweep out apollo and graphql, switch to pnpm

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	endOfLine: 'auto',
+	htmlWhitespaceSensitivity: 'css',
+	printWidth: 80,
+	proseWrap: 'preserve',
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
-import VueScrollTo from 'vue-scrollto'
+import { createApp } from 'vue';
+import router from './router';
+import App from './App.vue';
+import VueScrollTo from 'vue-scrollto';
 
-import logout from './plugins/logout'
-import { AutoFocus } from './directives/autofocus'
-import { AutoResize } from './directives/autoresize'
-import { titlize, symbolize, cruiseTo } from './plugins/utils'
+import logout from './plugins/logout';
+import { AutoFocus } from './directives/autofocus';
+import { AutoResize } from './directives/autoresize';
+import { titlize, symbolize, cruiseTo } from './plugins/utils';
 
-import './assets/styles/main.css'
+import './assets/styles/main.css';
 
-const app = createApp(App)
-app.directive('scroll-to', VueScrollTo)
-app.directive('autoresize', AutoResize)
-app.directive('autofocus', AutoFocus)
+const app = createApp(App);
+app.directive('scroll-to', VueScrollTo);
+app.directive('autoresize', AutoResize);
+app.directive('autofocus', AutoFocus);
 
+// prettier-ignore
 app
 	.use(router)
 	.use(logout)
 	.use(symbolize)
 	.use(titlize)
 	.use(cruiseTo)
-	.mount('#bollard')
+	.mount('#bollard');
