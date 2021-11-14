8a50eff6fd6650d4a326fed44f39bc76c70efacf
Kerry Snyder <kerry@atmanaut.me>
2021-11-13 23:38:26 -0800

    make sure service worker makes it to work let prod builds be simpler than local builds

--- workbox.config.js
+++ workbox.config.js
@@ -3,4 +3,5 @@ module.exports = {
 	globPatterns: ['**/*.{svg,jpg,xml,png,ico,json}'],
 	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/, /^source/],
 	maximumFileSizeToCacheInBytes: 4800000,
+	swDest: 'dist/sw.js',
 };
