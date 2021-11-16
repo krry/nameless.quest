d14113d0d3d909e2b9fa60cc238d61167b5d345c
Kerry Snyder <kerry@atmanaut.me>
2021-11-16 12:23:01 -0800

    build(versioning): use standard-version to automatically bump tag and commit a release

---
+++ .versionrc.js
@@ -0,0 +1,15 @@
+export default {
+	header: 'Changelog', // the header of the changelog
+	types: [
+		{ type: 'feat', section: 'Features' },
+		{ type: 'fix', section: 'Bug Fixes' },
+		{ type: 'chore', hidden: true },
+		{ type: 'docs', hidden: true },
+		{ type: 'style', hidden: true },
+		{ type: 'refactor', hidden: true },
+		{ type: 'perf', hidden: true },
+		{ type: 'test', hidden: true },
+	],
+	userUrlFormat: '{{host}}/{{user}}',
+	releaseCommitMessageFormat: 'chore(release): {{currentTag}}',
+};
