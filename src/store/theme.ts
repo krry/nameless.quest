b6947eb2a0bc9c949626bd8a33d131d6610306ff
Kerry Snyder <kerry@atmanaut.me>
2022-01-29 00:04:58 -0800

    fix(theme): stop endless while loop when applying theme
    that bug went on too long
    
    123

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -7,7 +7,7 @@ function checkThemePrefs() {
 	let mediaPref, dayOrNight;
 	const tiempo = new Date().getHours();
 	const savedTheme = cached.theme;
-	// console.log('theme saved was', savedTheme)
+	console.log('theme saved was', savedTheme);
 	if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
 		mediaPref = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : defTheme;
 	}
@@ -17,23 +17,25 @@ function checkThemePrefs() {
 
 export const activeTheme = ref<string>(checkThemePrefs());
 
-function removeGroupOfClassesFromEl(element: HTMLElement, className: string) {
-	const classList = element.classList;
-	while (classList.length > 0) {
-		const cls = classList.item(0);
-		if (cls === null) return;
-		if (cls.includes(className)) {
-			classList.remove(cls);
+// removes all classes containing the string 'theme' from the element passed as a parameter
+function removeGroupOfClassesFromEl(el: HTMLElement | null, str: string) {
+	if (el) {
+		const classes = el.classList;
+		for (let i = 0; i < classes.length; i++) {
+			if (classes[i].includes(str)) {
+				classes.remove(classes[i]);
+			}
 		}
 	}
 }
 
 function applyTheme(theme: string) {
-	const doc = document.documentElement;
+	console.log('applying theme:', theme);
+	const doc = document?.documentElement;
+	console.log('applying theme to doc');
 	removeGroupOfClassesFromEl(doc, 'theme');
 	doc.classList.add(`theme-${theme}`);
 	doc.setAttribute('data-theme', theme);
-	console.log('applying theme:', theme);
 	cache('theme', theme);
 	return theme;
 }
@@ -41,15 +43,21 @@ function applyTheme(theme: string) {
 export function setTheme(title: string): void {
 	console.log('setting theme to', title);
 	const titles = THEMES.map(t => t.title);
+	console.log('titles of themes', titles);
+
 	// if the title argument is one of the theme titles,
-	if (titles.indexOf(title) !== -1) {
-		applyTheme(title);
+	if (titles.some(t => t === title)) {
 		console.log('saves the arg as the active theme:', title);
+		applyTheme(title);
 		activeTheme.value = title;
+		return;
+	} else {
+		console.log('not the title of a theme');
 	}
 }
 
 export function nextTheme(): void {
+	if (!activeTheme.value) return;
 	// determines index of current active themes
 	console.log('activeTheme.value', activeTheme.value);
 	const currentIndex = THEMES.map(t => t.title).indexOf(activeTheme.value);
